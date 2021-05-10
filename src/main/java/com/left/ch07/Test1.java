da22f20f001b1098db78b93a9048fbfa036abdf9
linnanpeng <linnanpeng@gimc.cn>
2021-05-10 17:26:17 +0800

    ch07

---
+++ src/main/java/com/left/ch07/Test1.java
@@ -0,0 +1,44 @@
+package com.left.ch07;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.text.SimpleDateFormat;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.TemporalAccessor;
+
+@Slf4j(topic = "c.Test1")
+public class Test1 {
+
+    public static void main(String[] args) {
+
+        //test1();
+        test2();
+
+    }
+
+    private static void test1() {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        for (int i = 0; i < 10; i++) {
+            new Thread(() -> {
+                synchronized (sdf) {
+                    try {
+                        log.debug("{}", sdf.parse("1951-04-21"));
+                    } catch (Exception e) {
+                        log.error("{}", e);
+                    }
+                }
+            }).start();
+        }
+    }
+
+    private static void test2() {
+        DateTimeFormatter stf = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        for (int i = 0; i < 10; i++) {
+            new Thread(() -> {
+                TemporalAccessor parse = stf.parse("1951-04-21");
+                log.debug("{}", parse);
+            }).start();
+        }
+    }
+
+}
